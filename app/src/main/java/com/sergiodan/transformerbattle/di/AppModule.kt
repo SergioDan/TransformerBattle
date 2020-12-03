84fd36f5a1b94cacf5c8c6ad531a0f92a2977fde
SergioDan <danielkrad@gmail.com>
2020-12-03 01:07:51 -0800

    add initial configuration of the network components

--- app/src/main/java/com/sergiodan/transformerbattle/di/AppModule.kt
+++ app/src/main/java/com/sergiodan/transformerbattle/di/AppModule.kt
@@ -1,9 +1,39 @@
 package com.sergiodan.transformerbattle.di
 
+import com.google.gson.Gson
+import com.sergiodan.transformerbattle.BuildConfig
+import com.sergiodan.transformerbattle.data.services.TransformerService
+import dagger.Lazy
 import dagger.Module
-
+import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
-abstract class AppModule {
+class AppModule {
 
+    @Provides
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor =
+        HttpLoggingInterceptor().apply {
+            level = if (BuildConfig.DEBUG) {
+                HttpLoggingInterceptor.Level.BODY
+            } else {
+                HttpLoggingInterceptor.Level.NONE
+            }
+        }
+
+    @Provides
+    @Singleton
+    fun provideGson(): Gson = Gson()
+
+    @Provides
+    @Singleton
+    fun provideGsonConverterFactory(gson: Gson): GsonConverterFactory =
+        GsonConverterFactory.create(gson)
+
+    @Provides
+    fun provideOkHttpClient(interceptor: HttpLoggingInterceptor): OkHttpClient =
+        OkHttpClient.Builder().addInterceptor(interceptor).build()
 }
