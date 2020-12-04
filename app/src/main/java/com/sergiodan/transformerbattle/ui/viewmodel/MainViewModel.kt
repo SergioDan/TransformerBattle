bc00866834a309b05b1441cf964101e603261c7a
SergioDan <danielkrad@gmail.com>
2020-12-03 22:17:11 -0800

    Add viewmodel for the main activity.

---
+++ app/src/main/java/com/sergiodan/transformerbattle/ui/viewmodel/MainViewModel.kt
@@ -0,0 +1,25 @@
+package com.sergiodan.transformerbattle.ui.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.sergiodan.transformerbattle.data.DataManager
+import com.sergiodan.transformerbattle.data.model.Transformer
+import javax.inject.Inject
+
+class MainViewModel @Inject internal constructor(): ViewModel() {
+
+    @Inject
+    lateinit var dataManager: DataManager
+
+    private var _transformersLiveData = MutableLiveData<List<Transformer>>().also {
+        it.value = listOf()
+    }
+
+    val transformers: LiveData<List<Transformer>> = _transformersLiveData
+
+    fun getTransformers() {
+        _transformersLiveData = dataManager.getTransformers()
+    }
+
+}
