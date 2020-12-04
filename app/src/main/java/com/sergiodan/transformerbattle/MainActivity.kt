36f842414d65c2544cf8bffa964f17e99810a033
SergioDan <danielkrad@gmail.com>
2020-12-03 22:19:21 -0800

    Update activity to support injection. Inject viewmodel.

--- app/src/main/java/com/sergiodan/transformerbattle/MainActivity.kt
+++ app/src/main/java/com/sergiodan/transformerbattle/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.sergiodan.transformerbattle
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import com.github.ajalt.timberkt.d
+import com.sergiodan.transformerbattle.data.model.toMap
+import com.sergiodan.transformerbattle.ui.viewmodel.MainViewModel
+import dagger.android.support.DaggerAppCompatActivity
+import javax.inject.Inject
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : DaggerAppCompatActivity() {
+
+    @Inject
+    lateinit var mainViewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mainViewModel.getTransformers()
+        mainViewModel.transformers.observe(this, Observer {
+            it.forEach {
+                d { "Transformer=${it.toMap()}" }
+            }
+        })
     }
 }
