10117ed49da6c5cf89bddd1be63b0a2ce5138a81
SergioDan <danielkrad@gmail.com>
2020-12-06 23:16:00 -0800

    Add adapter, viewholder to show the saved transformers

--- app/src/main/java/com/sergiodan/transformerbattle/ui/fragments/BotsFragment.kt
+++ app/src/main/java/com/sergiodan/transformerbattle/ui/fragments/BotsFragment.kt
@@ -6,9 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.github.ajalt.timberkt.d
 import com.sergiodan.transformerbattle.data.model.toMap
 import com.sergiodan.transformerbattle.databinding.FragmentBotsBinding
+import com.sergiodan.transformerbattle.ui.adapter.TechnicalSpecificationAdapter
+import com.sergiodan.transformerbattle.ui.adapter.TransformersAdapter
 import com.sergiodan.transformerbattle.ui.viewmodel.MainViewModel
 import dagger.android.support.DaggerFragment
 import javax.inject.Inject
@@ -18,20 +21,16 @@ class BotsFragment: DaggerFragment() {
     @Inject
     lateinit var mainViewModel: MainViewModel
 
+    private lateinit var adapter: TransformersAdapter
+    private lateinit var binding: FragmentBotsBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         super.onCreateView(inflater, container, savedInstanceState)
-        val binding = FragmentBotsBinding.inflate(inflater, container, false)
-
-        mainViewModel.getTransformers()
-        mainViewModel.transformers.observe(viewLifecycleOwner, Observer {
-            it.forEach {
-                d { "Transformer=${it.toMap()}" }
-            }
-        })
+        binding = FragmentBotsBinding.inflate(inflater, container, false)
 
         binding.llFabBrawl.setOnClickListener {
             findNavController()
@@ -39,5 +38,28 @@ class BotsFragment: DaggerFragment() {
         }
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setRecyclerView()
+        startRequest()
+    }
+
+    private fun setRecyclerView() {
+        val recyclerView = binding.rvBots
+        recyclerView.layoutManager = LinearLayoutManager(activity)
+        adapter = TransformersAdapter(listOf())
+        recyclerView.adapter = adapter
+    }
+
+    private fun startRequest() {
+        mainViewModel.getTransformers()
+        mainViewModel.transformers.observe(viewLifecycleOwner, Observer {
+            it.forEach {
+                d { "Transformer=${it.toMap()}" }
+            }
+            adapter.updateList(it)
+        })
     }
 }
---
+++ app/src/main/java/com/sergiodan/transformerbattle/ui/views/TransformerViewHolder.kt
@@ -0,0 +1,30 @@
+package com.sergiodan.transformerbattle.ui.views
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.sergiodan.transformerbattle.R
+import com.sergiodan.transformerbattle.data.model.Transformer
+
+class TransformerViewHolder(inflater: LayoutInflater, parent: ViewGroup):
+    RecyclerView.ViewHolder(inflater.inflate(R.layout.view_holder_transformer)) {
+
+    private var nameTextView: TextView
+    private var imageTeamView: ImageView
+
+    init {
+        nameTextView = itemView.findViewById(R.id.tv_name)
+        imageTeamView = itemView.findViewById(R.id.iv_alliance)
+    }
+
+    fun bind(transformer: Transformer) {
+        nameTextView.text = transformer.name
+        Glide.get(itemView.context)
+        
+    }
+
+
+}
