d379177ed9286b15e2053710ec9367a51a0e66f2
SergioDan <danielkrad@gmail.com>
2020-12-07 17:47:47 -0800

    Integrate new custom view in the view holder.

--- app/src/main/java/com/sergiodan/transformerbattle/ui/views/TransformerViewHolder.kt
+++ app/src/main/java/com/sergiodan/transformerbattle/ui/views/TransformerViewHolder.kt
@@ -1,5 +1,6 @@
 package com.sergiodan.transformerbattle.ui.views
 
+import android.graphics.PointF
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.ImageView
@@ -8,16 +9,32 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.sergiodan.transformerbattle.R
 import com.sergiodan.transformerbattle.data.model.Transformer
+import com.sergiodan.transformerbattle.ui.fragments.CreateTransformerFragment
 
 class TransformerViewHolder(inflater: LayoutInflater, parent: ViewGroup):
     RecyclerView.ViewHolder(inflater.inflate(R.layout.view_holder_transformer, parent, false)) {
 
     private var nameTextView: TextView
     private var imageTeamView: ImageView
+    private var chartView: LineChartView
+
+    private val labels by lazy {
+        listOf(
+            itemView.context.getString(R.string.strength),
+            itemView.context.getString(R.string.intelligence),
+            itemView.context.getString(R.string.speed),
+            itemView.context.getString(R.string.endurance),
+            itemView.context.getString(R.string.rank),
+            itemView.context.getString(R.string.courage),
+            itemView.context.getString(R.string.firepower),
+            itemView.context.getString(R.string.skill)
+        )
+    }
 
     init {
         nameTextView = itemView.findViewById(R.id.tv_name)
         imageTeamView = itemView.findViewById(R.id.iv_alliance)
+        chartView = itemView.findViewById(R.id.lcv_chart)
     }
 
     fun bind(transformer: Transformer) {
@@ -26,7 +43,28 @@ class TransformerViewHolder(inflater: LayoutInflater, parent: ViewGroup):
             .with(itemView.context)
             .load(transformer.teamIcon)
             .into(imageTeamView)
-        
+
+        getYCoordinates(transformer).apply {
+            chartView.setCoordinatesAndLabels(this.toTypedArray(), labels.toTypedArray())
+        }
+    }
+
+
+    private fun getYCoordinates(transformer: Transformer): List<Float> {
+        val listSpecs = mutableListOf<Float>()
+        listSpecs.add(calculateCoordinateX(transformer.strength))
+        listSpecs.add(calculateCoordinateX(transformer.intelligence))
+        listSpecs.add(calculateCoordinateX(transformer.speed))
+        listSpecs.add(calculateCoordinateX(transformer.endurance))
+        listSpecs.add(calculateCoordinateX(transformer.rank))
+        listSpecs.add(calculateCoordinateX(transformer.courage))
+        listSpecs.add(calculateCoordinateX(transformer.firepower))
+        listSpecs.add(calculateCoordinateX(transformer.skill))
+        return listSpecs
+    }
+
+    private fun calculateCoordinateX(score: Int): Float {
+        return 80f - ((score.toFloat()/10f)*80f)
     }
 
 
